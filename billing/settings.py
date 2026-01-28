b06ed80354402ea0381dc6c4f88c06753f770249
niharadhanesh <niharadhanesh875@gmail.com>
2026-01-28 15:09:04 +0530

    added billing details

--- billing/settings.py
+++ billing/settings.py
@@ -139,11 +139,12 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email Configuration
+import os
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # For Gmail
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'niharadhanesh875@gmail.com'
-EMAIL_HOST_PASSWORD = 'pvtq nvdi gsoy zchc'  # Use App Password for Gmail
-DEFAULT_FROM_EMAIL = 'MediCare Pharmacy <niharadhanesh875@gmail.com>'
-SITE_URL = 'http://localhost:8000'  # Change in production
+
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
